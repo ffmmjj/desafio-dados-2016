1ff0993f61640e83c2a16d0596311714eabcd44c
Felipe Martins <ffmmjj+github@gmail.com>
2016-10-12 13:16:24 -0300

    Algumas correções no carregamento dos dados

--- src/carregamento/preprocessamento_escola_2013.py
+++ src/carregamento/preprocessamento_escola_2013.py
@@ -1,5 +1,9 @@
-import pandas as pd
+import os
 
+import pandas as pd
+import numpy as np
+
+from sklearn.cross_validation import train_test_split
 from src.carregamento.mapeamentos import dicionario_questoes_nomes_escola
 
 
@@ -18,6 +22,7 @@ def filter_questoes_recursos(df):
 def impute_missing_values(df, column):
     pass
 
+
 _escolas_2013_raw_pd = remove_nao_preenchidos(
     pd.read_csv(
         '../dados/prova_brasil_2013/TS_ESCOLA.csv'
@@ -29,4 +34,8 @@ _escolas_2013_raw_preenchido_pd = _escolas_2013_raw_pd[_escolas_2013_raw_pd['IN_
 _escolas_2013_raw_publicas_pd = _escolas_2013_raw_preenchido_pd[_escolas_2013_raw_preenchido_pd['ID_DEPENDENCIA_ADM'] != 4]
 _escolas_2013_raw_publicas_nao_federais = _escolas_2013_raw_publicas_pd[_escolas_2013_raw_publicas_pd['ID_DEPENDENCIA_ADM'] != 1]
 
-escolas_2013_pd = _escolas_2013_raw_publicas_nao_federais
+
+escolas_2013_pd_train_df, escolas_2013_pd_test = train_test_split(_escolas_2013_raw_publicas_nao_federais,
+                                                                  test_size=0.25, random_state=42)
+
+escolas_2013_pd = escolas_2013_pd_train_df
--- src/preprocessamento/codificacao.py
+++ src/preprocessamento/codificacao.py
@@ -2,6 +2,6 @@
 def codificar_questoes(questionario_pd):
     questionario_tratado_pd = questionario_pd.copy()
     for questoes in questionario_tratado_pd.filter(regex='Q_.*').columns:
-        questionario_tratado_pd[questoes] = questionario_pd[questoes].map({'A': 3, 'B': 2, 'C': 1, 'D': 0})
+        questionario_tratado_pd[questoes] = questionario_pd[questoes].map({'A': 3, 'B': 2, 'C': 1, 'D': 0}).fillna(0)
 
     return questionario_tratado_pd
